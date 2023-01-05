26de6c0c3f608b31d83a3bdd63c2d922f0ee3323
Jeremias Ferrao <feronest@yahoo.com>
2023-01-05 03:44:34 +0100

    Invalid Code

---
+++ constants.py
@@ -0,0 +1,54 @@
+# All Categories on the website
+# Fruits and vegetables do not have nutritional values
+CATEGORIES = [
+    "aardappelen,-groente-en-fruit/aardappelen",
+    # "aardappelen,-groente-en-fruit/groente",
+    # "aardappelen,-groente-en-fruit/groentemix-en-stamppot",
+    # "aardappelen,-groente-en-fruit/sla-en-salade",
+    # "aardappelen,-groente-en-fruit/pitten-en-zuidvruchten",
+    # "vlees,-vis,-vegetarisch",
+    # "verse-maaltijden-en-gemak",
+    # "brood-en-gebak",
+    # "vleeswaren,-kaas-en-tapas",
+    # "zuivel,-eieren,-boter",
+    # "conserven,-soepen,-sauzen,-olien",
+    # "wereldkeukens,-kruiden,-pasta-en-rijst",
+    # "ontbijt,-broodbeleg-en-bakproducten",
+    # "koek,-snoep,-chocolade-en-chips",
+    # "fris,-sap,-koffie,-thee",
+]
+
+# Require a user agent to access the website
+AGENT = {"User-Agent":'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36'}
+
+# All the nutritional values that are extracted from the website
+# Not required anymore
+NUTRIENT_INFO = [
+    'Name',
+    'Price', 
+    'Category',
+    'Subcategory', 
+    'Servings', 
+    'Serving Size', 
+    'Calories', 
+    'Carbohydrates', 
+    'Sugars', 
+    'Fat', 
+    'Saturated Fat', 
+    'Unsaturated Fat', 
+    'Fiber', 
+    'Protein',
+    'Salt',
+    'Vitamin A',
+    'Vitamin B1',
+    'Vitamin B2',
+    'Vitamin B3',
+    'Vitamin B5',
+    'Vitamin B6',
+    'Vitamin B11',
+    'Vitamin B12',
+    'Vitamin C',
+    'Vitamin D',
+    'Vitamin E',
+    'Vitamin K'
+]
---
+++ scraper.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import requests
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import numpy as np
+import time
+import re
+
+# Get the categories of food items and the nutrients we want to extract
+from constants import *
+
+# Finds the first number in a string
+def find_first_number(text):
+  # Find the first occurrence of a number in the text
+  match = re.search(r'\b\d+(?:\.\d+)?\b', text)
+  if match:
+    # Return the number if found
+    return float(match.group())
+  else:
+    # Return None if no number was found
+    return None
+
+
+# Extracts the product information from the HTML content on each page
+def extract_product_info(product_url, category):
+
+    # Wait for 1 second to prevent overloading the server
+    time.sleep(1)
+
+    data = {}
+
+    try:
+        s = HTMLSession()
+        response = s.get(product_url, headers=AGENT, timeout=15)
+        html = response.html.render().text
+    except requests.exceptions.ReadTimeout:
+        print("Time out for product, skipping...")
+        return
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+     # Get the name of the product
+    name = soup.find('h1').text
+    
+    # Print the name for logging progress
+    print(name)
+    data['Name'] = name
+    data['Category'] = category
+
+    table = soup.find('tbody').find_all('tr')
+
+    for row in table:
+
+        details = row.find_all('td')
+
+        if (len(details) < 2):
+            continue
+
+        nutrient = details[0].text
+
+        if not nutrient:
+            nutrient = 'Joules'
+
+        value = find_first_number(details[1].text)
+
+        data[nutrient] = value
+
+    return data
+
+# Gets all the products from a category
+def parse_category(dataframe, base_url, category):
+
+    print(f"Began category {category}")
+
+    # Represents the number of products to skip
+    offset = 0
+
+    # Create temp variable for stopping loop by checking if duplication occurs
+    temp = None
+
+    while (True):
+
+        print(f"Page: {offset // 24 + 1}")
+
+        # Request the html content of the page and parse it
+        # Retry if the request times out
+        try:
+            s = HTMLSession()
+            response = s.get(f"{base_url}/producten/{category}?offset={offset}", headers=AGENT, timeout=15)
+            html = response.html.render().text
+        except requests.exceptions.ReadTimeout:
+            print("Time out for page, retrying...")
+            continue
+
+        soup = BeautifulSoup(html, 'html.parser')
+
+        # Get href of all the products
+        products = soup.find_all(attrs={"analytics-tag" : "product card"})
+
+        # Stop if the product page starts looping
+        if (temp == products):
+            print(f"Finished category {category}")
+            return
+            
+
+        # Loop through all the products
+        for product in products:
+            product_addend = product.find('a').get('href')
+            product_url = base_url + product_addend
+            
+            info = extract_product_info(product_url, category)
+
+            if info:
+                dataframe.append(info)
+        
+        temp = products.copy()
+        
+        # There are 24 products per page
+        offset += 24
+    
+
+def main():
+
+    # Set website URL of Albert Hijn
+    url = 'https://www.jumbo.com'
+    nutritional_information = []
+
+    # Loop through all the categories
+    for category in CATEGORIES:
+        parse_category(nutritional_information, url, category)
+
+    # Create a dataframe from the nutritional information
+    df = pd.DataFrame(nutritional_information)
+
+    df.to_csv('groceries.csv', index=False)
+
+
+if __name__ == '__main__':
+    main()
